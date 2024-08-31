478ad660902d2aa185f7842b7ca2a909ee453bd4
quaktuss <mathieupheron1@gmail.com>
2024-09-01 01:26:02 +0200

    fix: add configuration to install SSM agent

--- terraform/modules/compute/userdata.sh
+++ terraform/modules/compute/userdata.sh
@@ -3,6 +3,12 @@
 sudo apt update
 sudo apt upgrade -y
 
-echo "Created by dhole on $(date +%d) $(date +%A) at $(date +%T)"
+echo "Created by dhole on $(date +%d) $(date +%A) at $(date +%T)" >> dhole.txt
+
+## Install SSM Agent
+sudo snap install amazon-ssm-agent --classic
+sudo snap start amazon-ssm-agent
 
 sudo shutdown -P +180
+
+
