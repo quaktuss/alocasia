8f76c8404c58cb8e7955ff808ba557bd0ef507e7
quaktuss <mathieupheron1@gmail.com>
2024-09-01 01:31:16 +0200

    fix: remove keypair and add name_prefix variable

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -1,16 +1,14 @@
 resource "aws_launch_template" "custom_ubuntu_2404" {
-  name                                 = "default-launchconfig"
+  name                                 = "${var.name_prefix}-ltemplate"
   image_id                             = data.aws_ami.ubuntu.id
   instance_type                        = var.instance_type
   instance_initiated_shutdown_behavior = var.terminated_instance
-  user_data                            = base64encode("${templatefile("${var.default_user_data}", {})}${var.custom_user_data}")
-  key_name                             = data.aws_key_pair.default_test_keypair.key_name
-
+  user_data                            = base64encode("${templatefile("${local.default_user_data}", {})}${var.custom_user_data}")
 }
 
 # Default ASG setting after deploy
 resource "aws_autoscaling_group" "asg-default" {
-  name               = var.autoscaling_group_name
+  name               = "${var.name_prefix}-asg"
   availability_zones = ["eu-west-1a"]
   min_size           = 0
   desired_capacity   = 1
@@ -22,8 +20,8 @@ resource "aws_autoscaling_group" "asg-default" {
 }
 
 # Auto scaledown ASG
-resource "aws_autoscaling_schedule" "name" {
-  scheduled_action_name  = "auto-scaledown"
+resource "aws_autoscaling_schedule" "scaledown" {
+  scheduled_action_name  = "0a-dhole-auto-scaledown"
   autoscaling_group_name = aws_autoscaling_group.asg-default.name
   recurrence             = var.instance_schedule
   max_size               = 0
