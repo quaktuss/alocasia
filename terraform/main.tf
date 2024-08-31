09c3b31a53107a1dc0a0eaa8c57f9b48808e1929
quaktuss <mathieupheron1@gmail.com>
2024-09-01 01:31:49 +0200

    fix: add name_prefix and remove autoscaling_group_name

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 module "ec2_custom" {
-  source                 = "./modules/compute"
-  count                  = length(var.asg_compute)
-  custom_user_data       = templatefile("${var.asg_compute[count.index].custom_user_data_filename}", {})
-  autoscaling_group_name = var.asg_compute[count.index].asg_name
+  source           = "./modules/compute"
+  count            = length(var.asg_compute)
+  custom_user_data = templatefile("${var.asg_compute[count.index].custom_user_data_filename}", {})
+  name_prefix      = "${local.name_prefix}-${var.asg_compute[count.index].compute_name_suffix}"
 }
